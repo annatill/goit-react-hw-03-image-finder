f7044a40a9ca4170f8d1fb60f4bd96bdf1021a58
annatill <anna.tataurova.95@gmail.com>
2023-05-08 20:20:24 +0200

    create react

---
+++ src/components/App/App.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+import { Container } from './App.styled';
+
+export class App extends Component {
+  render() {
+    return <Container></Container>;
+  }
+}
---
+++ src/components/App/App.styled.jsx
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 20px;
+  text-align: -webkit-center;
+  padding: 30px;
+  width: 800px;
+  min-height: 100vh;
+  border-radius: 10px;
+`;
